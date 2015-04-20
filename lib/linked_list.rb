f9df7a56d33280639d0f83c346fb20ba19d49e27
Devin Readel <readeldj@gmail.com>
2015-04-20 09:26:10 -0500

    To 10a. Trying to refactor 9 though

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,25 +1,52 @@
 require_relative 'linked_list_item'
 
 class LinkedList
-	def initialize
+    attr_reader :size
+	# def size # same thing as AttrReader
+	# 	@size
+	# end
+	def initialize(*args)
 		@size = 0
-	end
-
-	def size
-		@size
+		@more_stuff = *args
 	end
 
 	def last
-		if @size == 0
-			@last
-		else
-		    @last = get(@size - 1)
+		unless @last_item.nil?
+			@last_item.payload
 		end
 	end
 
+	# def last  # this was my way, it worked, but not as pretty.
+	# 	if @size == 0
+	# 		@last
+	# 	else
+	# 	    @last = get(@size - 1)
+	# 	end
+	# end
+
 	def to_s
-		to_s = "| |"
+		if @first_item.nil?
+		    to_s = "| |"
+		else
+			to_s = "| #{@stuff} |"
+		end
 	end
+
+	# def to_s
+	# 	if @first_item.nil?
+	# 	    str = "| |"
+	# 	else
+	# 		str = "| "
+	# 		@size.times do |i|
+	# 		    str << get(i)
+	# 		    if i != 0
+	# 		        str << ", "
+	# 		    end
+	# 		end
+	# 		str << " |"
+	# 	end
+	# 	str
+	# end
 
 	def get(index)
 		raise IndexError, "yeah something" if index < 0 || index > @size
@@ -35,29 +62,17 @@ class LinkedList
 	end
 
 	def push(payload)
+		new_item = LinkedListItem.new(payload)
 	    if @first_item.nil?
-		  @first_item = LinkedListItem.new(payload)
+		  @first_item = new_item
 		  @last_item = @first_item
+		  @stuff = @last_item.payload
 		else
-			@last_item.next_item = LinkedListItem.new(payload)
+			@last_item.next_item = new_item
 			@last_item = @last_item.next_item
+            @stuff += ", " + @last_item.payload
 		end
+		#@last_item = new_item
 		@size += 1
 	end
 end
-
-
-
-
-# class LinkedList
-
-
-#   def push (value)
-
-#   end
-
-#   def get (index)
-#     get = "foo"
-#    end
-
-# end
--- test/test_linked_list.rb
+++ test/test_linked_list.rb
@@ -56,6 +56,22 @@ class LinkedListTest < MiniTest::Unit::TestCase
     end
   end
 
+  def test_06d_get_that_doesnt_exist
+    ll = LinkedList.new
+    ll.push("bar")
+    assert_raises IndexError do
+      ll.get(1)
+    end
+  end
+
+  def test_06d_get_that_doesnt_exist
+    ll = LinkedList.new
+    ll.push("bar")
+    assert_raises IndexError do
+      ll.get(2)
+    end
+  end
+
   def test_07a_new_list_size_is_0
     assert_equal(0, LinkedList.new.size)
   end
